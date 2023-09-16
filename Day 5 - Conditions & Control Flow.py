840a1ac26ae283b3d8324fa366654763ba635833
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-16 11:55:44 +0530

    Day 5 added

---
+++ Day 5 - Conditions & Control Flow.py
@@ -0,0 +1,26 @@
+>>> True
+>>> False
+
+>>> print(True == False)
+
+>>> True != False
+
+>>> 10 > 31
+
+>>> 94 < 123
+
+>>> 133 >= 133
+
+>>> 133 <= 201
+
+>>> "abc" == "ABC".lower()
+
+>>> "XyZ".upper() == "XYZ"
+
+>>> print(str(True).lower() == "true")
+
+>>> 1 > 10
+
+>>> print((3123 / 30) == 32)
+
+>>> 24 % 5 == 3
