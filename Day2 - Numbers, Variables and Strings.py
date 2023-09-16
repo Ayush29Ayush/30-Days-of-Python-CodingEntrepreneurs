f68cb899af1af3290ceb792c80ea4779bab200cf
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-16 10:54:46 +0530

    Day 1 and 2 added

---
+++ Day2 - Numbers, Variables and Strings.py
@@ -0,0 +1,58 @@
+# Integer
+x = 10
+
+# Float
+y = 3.14
+
+# Basic arithmetic operations
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+exponentiation = x ** 2
+
+print(addition,subtraction,multiplication,division,exponentiation)
+
+
+
+# Variables can store various data types
+name = "John"
+age = 30
+is_student = True
+
+# You can change the value of a variable
+age = 31
+
+# Variable naming conventions
+user_name = "Alice"  # Snake Case
+userName = "Bob"    # Camel Case (often used in Python)
+
+
+
+
+# Creating strings
+greeting = "hello, World!"
+message = 'python is great.'
+
+# String concatenation
+full_message = greeting + " " + message
+
+# String length
+length = len(full_message)
+
+# Accessing characters in a string
+first_char = full_message[0]
+last_char = full_message[-1]
+
+# Slicing a string
+substring = full_message[7:12]  # Extracts "World"
+
+print(full_message)
+print("Length:", length)
+print("First char:", first_char)
+print("Last char:", last_char)
+print("Substring:", substring)
+capitalized_message1 = greeting.capitalize()
+capitalized_message2 = message.upper()
+print(capitalized_message1)
+print(capitalized_message2)
